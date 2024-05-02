455a07cde47cd83cbe68d66f0d41f8c0af216ce4
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-02 08:26:12 +0300

    MongoDb connection and folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
-import express from "express";
-const app = express();
+import dotenv from "dotenv";
+import app from "./app.js";
+import mongoose from "mongoose";
 
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-  });
-
-
-// Start the server
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE;
 const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log(`MongoDB connected ${con.connection.host}`);
+  })
+  .catch((err) => {
+    console.log(`Error:${err.message}`);
+  });
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
