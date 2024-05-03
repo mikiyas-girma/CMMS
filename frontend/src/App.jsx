102e61a13fd0933838adb23758524eb5579bc9bc
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-05-03 11:12:05 +0300

    changed the title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
     return (
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold text-red-600 underline">
         Hello world!
       </h1>
     )
