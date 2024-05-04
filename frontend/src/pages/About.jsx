e72b538928586f68d5fd1cea1df6d638b1a4cb43
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-05-04 14:34:29 +0300

    installed react router and on the way to configuring routes

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Welcome to our website!</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+
+
+export default function Layout() {
+    return (
+        <div>
+            <Header />
+
+            <main>
+                <Suspense fallback={<div>loading ...</div>}>
+                    <Outlet />
+                </Suspense>
+            </main>
+            
+            <Footer />
+        </div>
+    )
+}
