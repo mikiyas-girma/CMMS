02ab065973de17d36a02e20f1ed7d624f95da842
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-09 07:33:12 +0300

    RegisterStoreOwner

---
+++ backend/Controller/userController.js
@@ -0,0 +1,17 @@
+import User from "../Models/UserModel";
+export default RegisterStoreOwner = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
