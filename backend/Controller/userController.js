a0cc9a3ba02d349d76fdcc762ea28b4a41c44e08
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-09 13:53:39 +0300

    Assigning role during registerind StoreOwner

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,5 +1,6 @@
 import User from "../Models/UserModel.js";
 export const RegisterStoreOwner = async (req, res) => {
+  req.body.role = "storeOwner";
   try {
     const user = await User.create(req.body);
     res.status(201).json({
