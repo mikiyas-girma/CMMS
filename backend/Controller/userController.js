0ce850317dd92c440631a45c14f081f4734e2982
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-13 09:17:32 +0300

    Blocking StoreOwner

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -56,6 +56,32 @@ export const UpdateStoreOwner = async (req, res, next) => {
     });
   }
 };
+export const BlockStoreOwner = async (req, res) => {
+  try {
+    if (req.body.password || req.body.passwordConfirm) {
+      throw new Error("This Endpoint is not for updating password ");
+    }
+    const user = await StoreOwner.findByIdAndUpdate(
+      req.params.id,
+      {
+        status: "inactive",
+      },
+      { new: true }
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
 export const DeleteStoreOwner = async (req, res) => {
   try {
     const user = await StoreOwner.findByIdAndDelete(req.params.id);
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,10 +3,15 @@ import {
   RegisterStoreOwner,
   getAllStoreOwner,
   UpdateStoreOwner,
+  BlockStoreOwner,
 } from "../Controller/userController.js";
 const userRouter = express.Router();
 
-userRouter.route("/").get(getAllStoreOwner).post(RegisterStoreOwner);
-userRouter.route("/:id").patch(UpdateStoreOwner).delete(RegisterStoreOwner);
+userRouter.route("/storeOwner/").get(getAllStoreOwner).post(RegisterStoreOwner);
+userRouter
+  .route("/storeOwner/:id")
+  .patch(UpdateStoreOwner)
+  .delete(RegisterStoreOwner);
+userRouter.route("/storeOwner/block/:id").patch(BlockStoreOwner);
 
 export default userRouter;
