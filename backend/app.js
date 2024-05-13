1fcba2449b1fde55b173cfe0167c1bf36a15a3bd
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-13 10:14:55 +0300

    Middleware for url not found

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import { AppError } from "./utils/AppError.js";
 
 const app = express();
 import userRouter from "./routes/userRouter.js";
@@ -7,5 +9,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "developement") app.use(morgan("dev"));
 
 app.use("/cmms/api/users", userRouter);
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 export default app;
