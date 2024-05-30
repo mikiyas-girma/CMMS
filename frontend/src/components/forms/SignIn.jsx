83c897dc9a60b9d0fbd43354dd74fda4649b4863
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-30 11:47:05 +0300

    login function

--- frontend/src/components/forms/SignIn.jsx
+++ frontend/src/components/forms/SignIn.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   Container,
   FormControl,
@@ -14,59 +14,62 @@ import {
   InputRightElement,
   Checkbox,
   Link,
-  Flex
-} from '@chakra-ui/react';
-
+  Flex,
+} from "@chakra-ui/react";
+import apiInstance from "../../utils/axios";
 const SignIn = () => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
 
   return (
-<Flex p={8} flex={1} align="center">
-        <Stack spacing={4}>
-          <Stack align="center">
-            <Heading fontSize="2xl">Sign in to your account</Heading>
-          </Stack>
-          <VStack
-            as="form"
-            spacing={8}
-            boxSize={{ base: 'xs', sm: 'sm', md: 'sm' }}
-            h="max-content !important"
-            bg={useColorModeValue('white', 'gray.700')}
-            rounded="lg"
-            boxShadow="lg"
-            p={{ base: 5, sm: 10 }}
-          >
-            <VStack spacing={4} w="100%">
-              <FormControl id="username">
-                <FormLabel>username</FormLabel>
-                <Input rounded="md" type="text" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input rounded="md" type="password" />
-              </FormControl>
-            </VStack>
-            <VStack w="100%">
-              <Stack direction="row" justifyContent="space-between" w="100%">
-                <Checkbox colorScheme="green" size="md">
-                  Remember me
-                </Checkbox>
-                <Link fontSize={{ base: 'md', sm: 'md' }}>Forgot password?</Link>
-              </Stack>
-              <Button
-                bgGradient="linear(to-l, #0ea5e9,#2563eb)"
-                _hover={{ bgGradient: 'linear(to-l, #0ea5e9,#2562eb)', opacity: 0.8 }}
-                color="white"
-                rounded="md"
-                w="100%"
-              >
-                Sign in
-              </Button>
-            </VStack>
-          </VStack>
+    <Flex p={8} flex={1} align="center">
+      <Stack spacing={4}>
+        <Stack align="center">
+          <Heading fontSize="2xl">Sign in to your account</Heading>
         </Stack>
-      </Flex>
+        <VStack
+          as="form"
+          spacing={8}
+          boxSize={{ base: "xs", sm: "sm", md: "sm" }}
+          h="max-content !important"
+          bg={useColorModeValue("white", "gray.700")}
+          rounded="lg"
+          boxShadow="lg"
+          p={{ base: 5, sm: 10 }}
+        >
+          <VStack spacing={4} w="100%">
+            <FormControl id="username">
+              <FormLabel>username</FormLabel>
+              <Input rounded="md" type="text" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input rounded="md" type="password" />
+            </FormControl>
+          </VStack>
+          <VStack w="100%">
+            <Stack direction="row" justifyContent="space-between" w="100%">
+              <Checkbox colorScheme="green" size="md">
+                Remember me
+              </Checkbox>
+              <Link fontSize={{ base: "md", sm: "md" }}>Forgot password?</Link>
+            </Stack>
+            <Button
+              bgGradient="linear(to-l, #0ea5e9,#2563eb)"
+              _hover={{
+                bgGradient: "linear(to-l, #0ea5e9,#2562eb)",
+                opacity: 0.8,
+              }}
+              color="white"
+              rounded="md"
+              w="100%"
+            >
+              Sign in
+            </Button>
+          </VStack>
+        </VStack>
+      </Stack>
+    </Flex>
   );
 };
 
