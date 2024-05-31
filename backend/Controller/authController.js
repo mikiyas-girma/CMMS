7d30b3a28b0635bc8d5c06685917f448c535d89b
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-31 17:58:22 +0300

    sweetalert2 during login successfully

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -51,11 +51,16 @@ export const login = asyncHandler(async (req, res, next) => {
 });
 export const protect = asyncHandler(async (req, res, next) => {
   let token;
-  // console.log("req", req);
 
-  if (req.cookies && req.cookies.jwt) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies && req.cookies.jwt) {
     token = req.cookies.jwt;
   }
+
   // console.log("Token:", req.cookies.jwt);
 
   if (!token) {
--- frontend/src/utils/login.js
+++ frontend/src/utils/login.js
@@ -1,11 +1,22 @@
 import apiInstance from "./axios";
-
+import Cookies from "js-cookie";
+export const setAuthUser = (token) => {
+  Cookies.set("jwt", token, {
+    expires: 1,
+    // secure: true,
+  });
+};
 export const login = async (email, password) => {
   try {
-    const data = await apiInstance.post("users/login", {
+    const { data } = await apiInstance.post("users/login", {
       email,
       password,
     });
+    // console.log("token", data?.token);
+
+    if (data?.status === "success") {
+      setAuthUser(data?.token);
+    }
     return { data, error: null };
   } catch (error) {
     console.log("er", error);
