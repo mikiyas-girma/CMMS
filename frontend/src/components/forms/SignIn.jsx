e92fe884f800adfa7e51389bd436b528a1da8122
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-31 22:17:44 +0300

    error message in login page styling updated

--- frontend/src/components/forms/SignIn.jsx
+++ frontend/src/components/forms/SignIn.jsx
@@ -86,7 +86,7 @@ const SignIn = () => {
               />
             </FormControl>
             {error && (
-              <p className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+              <p className="bg-red-100 border  text-red-700 px-4 py-2 rounded w-full">
                 {error}
               </p>
             )}
