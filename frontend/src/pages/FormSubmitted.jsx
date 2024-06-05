326592f4863e20c19789f0fae11cd7491112d72d
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-06-05 10:28:31 +0300

    Submitted Form Details page

---
+++ frontend/src/pages/FormSubmitted.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const FormSubmitted = ({ data, onCancel, onConfirm }) => {
+  const password = AutoGeneratePassword(data.email);
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-90 z-50 overflow-hidden">
+      <div className=" bg-white border border-gray-300 rounded-lg p-8 px-[100px] shadow-md relative ">
+        <h2 className="text-xl font-semibold mb-4">Form Submission Details</h2>
+        <p className="mb-2">
+          <strong>First Name:</strong> {data.Fname}
+        </p>
+        <p className="mb-2">
+          <strong>Last Name:</strong> {data.Lname}
+        </p>
+        <p className="mb-2">
+          <strong>Email:</strong> {data.email}
+        </p>
+        <p className="mb-4">
+          <strong>Phone:</strong> {data.phone}
+        </p>
+        <p className="mb-4">
+          <strong>Password:</strong> {password}
+        </p>
+        <div className="flex  space-x-4">
+          <button
+            className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+            onClick={onConfirm}
+          >
+            Confirm Registration
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormSubmitted;
