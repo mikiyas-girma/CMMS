05e4113c68058df9e5f4793876c28cdecfbf5010
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-06-14 16:09:43 +0300

    added new chart

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,35 +6,34 @@ import BarChart from '../charts/graphs/BarChart';
 import InventoryOverview from '../components/materials/InventoryOverview';
 import EmployeeOverview from '../components/employee/EmployeeOverview';
 import RecentNotifications from '../components/notifications/RecentNotifications';
+import MaterialAdditionsWithdrawalsChart from '../charts/graphs/MaterialAdditionsWithdrawalsChart';
 
 const Dashboard = () => {
     const location = useLocation();
     return (
-      <SidebarWithHeader>
-        <Box p="4">
-          <Grid
-            gridTemplateColumns={{ base: '1fr', md: '1fr 1fr', lg: '1fr .5fr 1fr'}}
-            templateRows={{ base: 'repeat(4, 1fr)', md: 'repeat(2, 1fr)' }}
-            gap={0}
-          >
-            <GridItem colSpan={{ base: 1, md: 3, lg: 2 }} bg="" p={0} boxShadow="sm">
-              <InventoryOverview />
-            </GridItem>
-            <GridItem colSpan={{ base: 1, md: 3, lg: 1 }} bg="" px={4} boxShadow="sm">
-              <Text>Chart</Text>
-              <LineChart />
-            </GridItem>
-            <GridItem colSpan={{ base: 1, md: 3, lg: 1 }} bg="" px={4} boxShadow="sm">
-              {/* <EmployeeOverview /> */}
-              <RecentNotifications />
-            </GridItem>
-            <GridItem colSpan={{ base: 1, md: 3, lg: 2 }} bg="" px={4} boxShadow="sm">
-              <Text>Materials</Text>
-              <BarChart />
-            </GridItem>
-          </Grid>
-        </Box>
-      </SidebarWithHeader>
+        <SidebarWithHeader>
+            <Box p="4">
+                <Grid
+                    gridTemplateColumns={{ base: '1fr', md: '1fr 1fr', lg: '1fr .5fr 1fr' }}
+                    templateRows={{ base: 'repeat(4, 1fr)', md: 'repeat(2, 1fr)' }}
+                    gap={0}
+                >
+                    <GridItem colSpan={{ base: 1, md: 3, lg: 2 }} bg="" p={0} boxShadow="sm">
+                        <InventoryOverview />
+                    </GridItem>
+                    <GridItem colSpan={{ base: 1, md: 3, lg: 1 }} boxShadow="sm" display="flex" justifyContent="center" alignItems="center">
+                        <LineChart />
+                    </GridItem>
+                    <GridItem colSpan={{ base: 1, md: 3, lg: 1 }} bg="" px={4} boxShadow="sm">
+                        <RecentNotifications />
+                    </GridItem>
+                    <GridItem colSpan={{ base: 1, md: 3, lg: 2 }} bg="" px={4} boxShadow="sm">
+                        {/* <BarChart /> */}
+                        <MaterialAdditionsWithdrawalsChart />
+                    </GridItem>
+                </Grid>
+            </Box>
+        </SidebarWithHeader>
     );
 };
 
