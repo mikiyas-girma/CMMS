a84093652b75814bc6aa01add8094e7b68418e40
Biniam batu <Biniambatu12@gmail.com>
2024-06-21 11:01:44 +0300

    Footer and about page

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,7 +1,8 @@
 import { Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
+    const location = useLocation()
     return (
       <>
       {/* <Flex bottom={0}  as="footer" py={8}
@@ -11,52 +12,29 @@ const Footer = () => {
       
 
                 <footer class="bg-white dark:bg-gray-900">
-                    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                        <div class="md:flex md:justify-between">
-                          <div class="mb-6 md:mb-0">
-                              <Link to="https://flowbite.com/" class="flex items-center">
-                              </Link>
-                          </div>
-                          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                              <div>
-                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white hover:underline"><Link to='about'>ABOUT US</Link></h2>
-                                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                                      <li class="mb-4">
-                                          
-                                      </li>
-                                      <li>
-                                          <Link to="https://tailwindcss.com/" class="hover:underline"></Link>
-                                      </li>
-                                  </ul>
-                              </div>
-                              <div>
-                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">CONTACT US</h2>
-                                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                                      <li class="mb-4">
-                                          <Link to="" class="hover:underline ">E-mail</Link>
-                                      </li>
-                                      <li>
-                                          <Link to="https://discord.gg/4eeurUVvTy" class="hover:underline"></Link>
-                                      </li>
-                                  </ul>
-                              </div>
-                              <div>
-                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                                      <li class="mb-4">
-                                          <Link to="#" class="hover:underline">Privacy Policy</Link>
-                                      </li>
-                                      <li>
-                                          <Link to="#" class="hover:underline">Terms &amp; Conditions</Link>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div>
-                      </div>
+                   
+
+                      
                       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                       <div class="sm:flex sm:items-center sm:justify-between">
-                          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 mx-auto"> © {new Date().getFullYear()} <Link to="/" class="hover:underline">CMMS</Link>. All Rights Reserved.
+                        
+                          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400  ml-180"> © {new Date().getFullYear()} <Link to="/" class="hover:underline">CMMS</Link>. All Rights Reserved.
                           </span>
+                          <div class="grid grid-cols-2  sm:gap-6 sm:grid-cols-4  mt-5">
+                              <div>
+                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white hover:underline"><Link to='about'>ABOUT US</Link></h2>
+                              </div>
+                              <div>
+                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white hover:underline"><Link to=''>CONTACT US</Link></h2>
+                              </div>
+                              <div>
+                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white hover:underline"><Link to=''>Privacy Policy</Link></h2>
+                              </div>
+                              <div>
+                                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white hover:underline"><Link to='about'>faq</Link></h2>
+                              </div>
+                          </div>
+                      
                           <div class="flex mt-4 sm:justify-center sm:mt-0">
                               <Link to="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
@@ -90,7 +68,7 @@ const Footer = () => {
                               </Link>
                           </div>
                       </div>
-                    </div>
+                    
             </footer>
 
 </>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,12 +1,85 @@
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Grid, GridItem, HStack, Heading, Image, Stack, StackDivider, Text } from "@chakra-ui/react";
 import SidebarWithHeader from "../components/sidebar/SidebarWithHeader";
 
 const About = () => {
     return (
         <SidebarWithHeader>
-        <div>
-            <h1>This is the About Page</h1>
-        </div>
-        </SidebarWithHeader>
+        <div><br /><br />
+            <Text fontSize='30px' color='blueblack' as='b' marginLeft='440px' marginTop='40px'  >About Us</Text><br />
+            <p class="mb-3 text-center text-gray-500 dark:text-gray-400">Welcome to our Construction and Material Store Management System, your one-stop solution for streamlining your construction business operations. We've developed this comprehensive platform to address the unique challenges faced by construction companies and material suppliers in today's fast-paced industry.</p>
+            <Text fontSize='20px' color='blueblack' as='b' marginLeft='450px' marginTop='40px'  >Our Mission</Text><br />
+            <p class="mb-3 text-center text-gray-500 dark:text-gray-400">Our mission is to empower construction professionals and material providers with the tools and insights they need to optimize their workflows, increase efficiency, and drive business growth. By combining cutting-edge technology, industry expertise, and a deep understanding of the construction ecosystem, we strive to revolutionize the way you manage your construction and material operations.</p>
+            <Text fontSize='20px' color='blueblack' as='b' marginLeft='450px' marginTop='40px'  >Key Features</Text><br />
+            <p class="mb-3 text-center text-gray-500 dark:text-gray-400"> Our Construction and Material Store Management System offers a wide range of features designed to meet the diverse needs of our customers:</p>
+    <HStack marginLeft='100px' marginRight='100px' marginBottom='50px' >
+             <Card  marginLeft='100px' >
+                        <CardBody  marginTop='20px'>
+                            <Stack divider={<StackDivider />} spacing='10' >
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Inventory Management
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                Effortlessly track and manage your inventory of construction materials, equipment, and supplies, ensuring you always have the right products on hand when you need them.
+                                </Text>
+                            </Box>
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Project Tracking
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                Monitor the progress of your ongoing construction projects, keep your team informed, and make data-driven decisions to ensure timely completion and profitability.
+                                </Text>
+                            </Box>
+                            <Box marginBottom='20px'>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Procurement and Ordering
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                Simplify the procurement process by integrating supplier catalogs, automating order placement, and streamlining invoice management.
+                                </Text>
+                            </Box>
+                            </Stack>
+                        </CardBody>
+                        </Card>
+        <Card marginRight='100px' gap='300px'>
+                        <CardBody gap='10px'marginTop='20px'>
+                            <Stack divider={<StackDivider />} spacing='10' columnGap='3'>
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Financial Reporting
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                Gain real-time insights into your financial performance, with comprehensive reports and analytics to help you make informed business decisions.                                </Text>
+                            </Box>
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Workforce Management
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                Efficiently schedule and coordinate your team's activities, track their productivity, and ensure optimal resource utilization across your construction projects.                                </Text>
+                            </Box>
+                            <Box marginBottom='20px'>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Customizable Dashboards
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                Tailor the user experience to your specific needs, with customizable dashboards and reporting tools that provide a clear, at-a-glance view of your business operations.                                </Text>
+                            </Box>
+                            </Stack>
+                        </CardBody>
+                        </Card>
+    </HStack>       
+            
+       
+                        <Text fontSize='20px' color='blueblack' as='b' marginLeft='450px'    > Our Commitment to Excellence</Text><br /><br />
+                        <p class="mb-3 text-center text-gray-500 dark:text-gray-400">At the core of our Construction and Material Store Management System is a relentless commitment to excellence. We've assembled a team of industry experts, software engineers, and customer service professionals who are dedicated to delivering exceptional solutions and providing unparalleled support to our clients.</p>
+                        <p class="mb-3 text-center text-gray-500 dark:text-gray-400">We understand the unique challenges faced by construction companies and material suppliers, and we've designed our system to address those challenges head-on. By continuously innovating and adapting to the evolving needs of the industry, we strive to be the trusted partner in your construction and material management journey.</p>
+                        <p class="mb-3 text-center text-gray-500 dark:text-gray-400">We invite you to explore the capabilities of our Construction and Material Store Management System and discover how it can transform your business. Contact us today to learn more and schedule a personalized demonstration.</p>
+  <br /><br />
+               </div>
+              
+    </SidebarWithHeader>
     );
 };
 
